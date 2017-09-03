5f808f594764d0b95d10e133d0d5c362ec3683b4
Noiker <vtankaxmasterok@gmail.com>
2017-09-03 17:00:26 +0300

    Web project

---
+++ stepWebAppExample/stepWebAppExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(stepWebAppExample.Startup))]
+namespace stepWebAppExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
